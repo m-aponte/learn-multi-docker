cc132b6f848447c9b387e819dd677632a81db84a
M A <maponte@libertymail.net>
2021-12-05 15:02:00 -0500

    Add render and submit functionality to Fib form in client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -25,14 +25,54 @@ class Fib extends Component {
     });
   }
 
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index,
+    });
+    this.setState({ index: '' });
+  };
+
+  renderSeenIndices() {
+    return this.state.seenIndices
+      .map(({ number }) => {
+        return number;
+      })
+      .join(', ');
+  }
+
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}.
+        </div>
+      );
+    }
+
+    return entries;
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Enter your index:</label>
-          <input />
+          <input
+            value={this.state.index}
+            onChange={(event) => this.setState({ index: event.target.value })}
+          />
           <button>Submit</button>
         </form>
+
+        <h3>Indices I have seen</h3>
+        {this.renderSeenIndices()}
+
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
       </div>
     );
   }
