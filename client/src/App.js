ab04c8fe766c55c90cc01c16f1a32b920c12c13e
M A <maponte@libertymail.net>
2021-12-05 19:00:30 -0500

    Remove exact routing and shrink header, remove label from client homepage

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,13 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
           <Routes>
-            <Route exact path="/" element={<Fib />} />
-            <Route exact path="/otherpage" element={<OtherPage />} />
+            <Route path="/" element={<Fib />} />
+            <Route path="/otherpage" element={<OtherPage />} />
           </Routes>
         </div>
       </div>
